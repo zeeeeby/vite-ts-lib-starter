790e23963223e47441cf6e581a778c159179e959
dmi-yushin <dmi-yushin@yandex-team.ru>
2025-10-19 21:28:22 +0300

    upd template

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import path from "path";
 import { defineConfig } from "vite";
 import packageJson from "./package.json";
 
-const getPackageName = () => {
-  return packageJson.name;
-};
-
 const getPackageNameCamelCase = () => {
   try {
-    return getPackageName().replace(/-./g, char => char[1].toUpperCase());
+    return packageJson.name
+      .replace('@pluskids/', '')
+      .replace(/-./g, char => char[1].toUpperCase());
   } catch {
     throw new Error("Name property in package.json is missing.");
   }
@@ -18,7 +16,7 @@ const getPackageNameCamelCase = () => {
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "./build/dist",
+    outDir: "./dist",
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: getPackageNameCamelCase(),
